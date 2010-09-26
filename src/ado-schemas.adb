709a556ca5e853448abad875f95905d25570de51
Stephane Carrez <Stephane.Carrez@gmail.com>
2010-09-26 16:57:29 +0000

    Simple model to describe database tables and columns

---
+++ src/ado-schemas.adb
@@ -0,0 +1,252 @@
+-----------------------------------------------------------------------
+--  ado.schemas -- Database Schemas
+--  Copyright (C) 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Unchecked_Deallocation;
+package body ADO.Schemas is
+
+   procedure Free is new
+     Ada.Unchecked_Deallocation (Object => Column,
+                                 Name   => Column_Definition);
+   procedure Free is new
+     Ada.Unchecked_Deallocation (Object => Table,
+                                 Name   => Table_Definition);
+   procedure Free is new
+     Ada.Unchecked_Deallocation (Object => Schema,
+                                 Name   => Schema_Access);
+
+
+   --  ------------------------------
+   --  Column Representation
+   --  ------------------------------
+
+   --  ------------------------------
+   --  Get the column name
+   --  ------------------------------
+   function Get_Name (Column : Column_Definition) return String is
+   begin
+      return To_String (Column.Name);
+   end Get_Name;
+
+   --  ------------------------------
+   --  Get the column type
+   --  ------------------------------
+   function Get_Type (Column : Column_Definition) return Column_Type is
+   begin
+      return Column.Col_Type;
+   end Get_Type;
+
+   --  ------------------------------
+   --  Get the default column value
+   --  ------------------------------
+   function Get_Default (Column : Column_Definition) return String is
+   begin
+      return To_String (Column.Default);
+   end Get_Default;
+
+   --  ------------------------------
+   --  Get the column collation (for string based columns)
+   --  ------------------------------
+   function Get_Collation (Column : Column_Definition) return String is
+   begin
+      return To_String (Column.Collation);
+   end Get_Collation;
+
+   --  ------------------------------
+   --  Check whether the column can be null
+   --  ------------------------------
+   function Is_Null (Column : Column_Definition) return Boolean is
+   begin
+      return Column.Is_Null;
+   end Is_Null;
+
+   --  ------------------------------
+   --  Check whether the column is an unsigned number
+   --  ------------------------------
+   function Is_Unsigned (Column : Column_Definition) return Boolean is
+   begin
+      return Column.Is_Unsigned;
+   end Is_Unsigned;
+
+   --  ------------------------------
+   --  Returns true if the column can hold a binary string
+   --  ------------------------------
+   function Is_Binary (Column : Column_Definition) return Boolean is
+   begin
+      return Column.Is_Binary;
+   end Is_Binary;
+
+   --  ------------------------------
+   --  Get the column length
+   --  ------------------------------
+   function Get_Size (Column : Column_Definition) return Natural is
+   begin
+      return Column.Size;
+   end Get_Size;
+
+   --  ------------------------------
+   --  Column iterator
+   --  ------------------------------
+
+   --  ------------------------------
+   --  Returns true if the iterator contains more column
+   --  ------------------------------
+   function Has_Element (Cursor : Column_Cursor) return Boolean is
+   begin
+      return Cursor.Current /= null;
+   end Has_Element;
+
+   --  ------------------------------
+   --  Move to the next column
+   --  ------------------------------
+   procedure Next (Cursor : in out Column_Cursor) is
+   begin
+      if Cursor.Current /= null then
+         Cursor.Current := Cursor.Current.Next_Column;
+      end if;
+   end Next;
+
+   --  ------------------------------
+   --  Get the current column definition
+   --  ------------------------------
+   function Element (Cursor : Column_Cursor) return Column_Definition is
+   begin
+      return Cursor.Current;
+   end Element;
+
+   --  ------------------------------
+   --  Table Representation
+   --  ------------------------------
+
+   --  ------------------------------
+   --  Get the table name
+   --  ------------------------------
+   function Get_Name (Table : Table_Definition) return String is
+   begin
+      return To_String (Table.Name);
+   end Get_Name;
+
+   --  ------------------------------
+   --  Get the column iterator
+   --  ------------------------------
+   function Get_Columns (Table : Table_Definition) return Column_Cursor is
+   begin
+      return Column_Cursor '(Current => Table.First_Column);
+   end Get_Columns;
+
+   --  ------------------------------
+   --  Find the column having the given name
+   --  ------------------------------
+   function Find_Column (Table : Table_Definition;
+                         Name  : String) return Column_Definition is
+      Column : Column_Definition := Table.First_Column;
+   begin
+      while Column /= null loop
+         if To_String (Column.Name) = Name then
+            return Column;
+         end if;
+         Column := Column.Next_Column;
+      end loop;
+      return null;
+   end Find_Column;
+
+   --  ------------------------------
+   --  Table iterator
+   --  ------------------------------
+
+   --  ------------------------------
+   --  Returns true if the iterator contains more tables
+   --  ------------------------------
+   function Has_Element (Cursor : Table_Cursor) return Boolean is
+   begin
+      return Cursor.Current /= null;
+   end Has_Element;
+
+   --  ------------------------------
+   --  Move to the next column
+   --  ------------------------------
+   procedure Next (Cursor : in out Table_Cursor) is
+   begin
+      if Cursor.Current /= null then
+         Cursor.Current := Cursor.Current.Next_Table;
+      end if;
+   end Next;
+
+   --  ------------------------------
+   --  Get the current table definition
+   --  ------------------------------
+   function Element (Cursor : Table_Cursor) return Table_Definition is
+   begin
+      return Cursor.Current;
+   end Element;
+
+   --  ------------------------------
+   --  Database Schema
+   --  ------------------------------
+
+   --  ------------------------------
+   --  Find a table knowing its name
+   --  ------------------------------
+   function Find_Table (Schema : Schema_Definition;
+                        Name   : String) return Table_Definition is
+      Table : Table_Definition;
+   begin
+      if Schema.Schema /= null then
+         Table := Schema.Schema.First_Table;
+         while Table /= null loop
+            if To_String (Table.Name) = Name then
+               return Table;
+            end if;
+            Table := Table.Next_Table;
+         end loop;
+      end if;
+      return null;
+   end Find_Table;
+
+   function Get_Tables (Schema : Schema_Definition) return Table_Cursor is
+   begin
+      if Schema.Schema = null then
+         return Table_Cursor '(Current => null);
+      else
+         return Table_Cursor '(Current => Schema.Schema.First_Table);
+      end if;
+   end Get_Tables;
+
+   procedure Finalize (Schema : in out Schema_Definition) is
+   begin
+      if Schema.Schema /= null then
+         declare
+            Table : Table_Definition;
+            Column : Column_Definition;
+         begin
+            loop
+               Table := Schema.Schema.First_Table;
+               exit when Table = null;
+               loop
+                  Column := Table.First_Column;
+                  exit when Column = null;
+                  Table.First_Column := Column.Next_Column;
+                  Free (Column);
+               end loop;
+               Schema.Schema.First_Table := Table.Next_Table;
+               Free (Table);
+            end loop;
+         end;
+         Free (Schema.Schema);
+      end if;
+   end Finalize;
+
+end ADO.Schemas;
---
+++ src/ado-schemas.ads
@@ -0,0 +1,202 @@
+-----------------------------------------------------------------------
+--  ado.schemas -- Database Schemas
+--  Copyright (C) 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Strings.Unbounded;
+with Ada.Finalization;
+with Util.Strings;
+package ADO.Schemas is
+
+   type Member_Names is array (Natural range <>) of Util.Strings.Name_Access;
+
+   type Class_Mapping (Count : Natural) is tagged record
+      Table   : Util.Strings.Name_Access;
+--        Name : Natural := 0;
+      Members : Member_Names (1 .. Count);
+   end record;
+
+   type Class_Mapping_Access is access constant Class_Mapping'Class;
+
+   --  Get the Ada type mapping for the column
+   type Column_Type is
+     (
+      T_UNKNOWN,
+
+      --  Boolean column
+      T_BOOLEAN,
+
+      T_TINYINT,
+      T_SMALLINT,
+      T_INTEGER,
+      T_LONG_INTEGER,
+
+      T_FLOAT,
+      T_DOUBLE,
+
+      T_DECIMAL,
+
+      T_ENUM,
+      T_SET,
+
+      T_TIME,
+      T_YEAR,
+      T_DATE,
+      T_DATE_TIME,
+      T_TIMESTAMP,
+
+      T_CHAR,
+      T_VARCHAR,
+      T_BLOB
+     );
+
+   --  ------------------------------
+   --  Column Representation
+   --  ------------------------------
+   --  Describes a column in a table.
+   type Column_Definition is private;
+
+   --  Get the column name
+   function Get_Name (Column : Column_Definition) return String;
+
+   --  Get the column type
+   function Get_Type (Column : Column_Definition) return Column_Type;
+
+   --  Get the default column value
+   function Get_Default (Column : Column_Definition) return String;
+
+   --  Get the column collation (for string based columns)
+   function Get_Collation (Column : Column_Definition) return String;
+
+   --  Check whether the column can be null
+   function Is_Null (Column : Column_Definition) return Boolean;
+
+   --  Check whether the column is an unsigned number
+   function Is_Unsigned (Column : Column_Definition) return Boolean;
+
+   --  Returns true if the column can hold a binary string
+   function Is_Binary (Column : Column_Definition) return Boolean;
+
+   --  Get the column length
+   function Get_Size (Column : Column_Definition) return Natural;
+
+   --  ------------------------------
+   --  Column iterator
+   --  ------------------------------
+   type Column_Cursor is private;
+
+   --  Returns true if the iterator contains more column
+   function Has_Element (Cursor : Column_Cursor) return Boolean;
+
+   --  Move to the next column
+   procedure Next (Cursor : in out Column_Cursor);
+
+   --  Get the current column definition
+   function Element (Cursor : Column_Cursor) return Column_Definition;
+
+   --  ------------------------------
+   --  Table Representation
+   --  ------------------------------
+   --  Describes a table in the database.  The table contains a list
+   --  of columns described by Column_Definition.
+   type Table_Definition is private;
+
+   --  Get the table name
+   function Get_Name (Table : Table_Definition) return String;
+
+   --  Get the column iterator
+   function Get_Columns (Table : Table_Definition) return Column_Cursor;
+
+   --  Find the column having the given name
+   function Find_Column (Table : Table_Definition;
+                         Name  : String) return Column_Definition;
+
+   --  ------------------------------
+   --  Table iterator
+   --  ------------------------------
+   type Table_Cursor is private;
+
+   --  Returns true if the iterator contains more tables
+   function Has_Element (Cursor : Table_Cursor) return Boolean;
+
+   --  Move to the next column
+   procedure Next (Cursor : in out Table_Cursor);
+
+   --  Get the current table definition
+   function Element (Cursor : Table_Cursor) return Table_Definition;
+
+   --  ------------------------------
+   --  Database Schema
+   --  ------------------------------
+
+   type Schema_Definition is limited private;
+
+   --  Find a table knowing its name
+   function Find_Table (Schema : Schema_Definition;
+                        Name   : String) return Table_Definition;
+
+   function Get_Tables (Schema : Schema_Definition) return Table_Cursor;
+
+private
+   use Ada.Strings.Unbounded;
+
+   type Column;
+   type Column_Definition is access all Column;
+
+   type Table;
+   type Table_Definition is access all Table;
+
+   type Schema;
+   type Schema_Access is access all Schema;
+
+   type Schema_Definition is new Ada.Finalization.Limited_Controlled with record
+      Schema : Schema_Access;
+   end record;
+
+   procedure Finalize (Schema : in out Schema_Definition);
+
+   type Column_Cursor is record
+      Current    : Column_Definition;
+   end record;
+
+   type Table_Cursor is record
+      Current    : Table_Definition;
+   end record;
+
+   type Column is record
+      Next_Column : Column_Definition;
+      Table       : Table_Definition;
+      Name        : Unbounded_String;
+      Default     : Unbounded_String;
+      Collation   : Unbounded_String;
+      Col_Type    : Column_Type   := T_VARCHAR;
+      Size        : Natural       := 0;
+      Is_Null     : Boolean       := False;
+      Is_Binary   : Boolean       := False;
+      Is_Unsigned : Boolean       := False;
+   end record;
+
+   type Table is record
+      Name         : Unbounded_String;
+      First_Column : Column_Definition;
+      Next_Table   : Table_Definition;
+   end record;
+
+   type Schema is record
+      --        Tables : Table_Definition;
+      First_Table : Table_Definition;
+   end record;
+
+end ADO.Schemas;
