28fc0b0e1f2bbf04db3358e0df76e7c49d594c3b
Stephane Carrez <Stephane.Carrez@gmail.com>
2010-09-26 15:49:24 +0000

    Define and implement session objects - Session for read access - Master session for read+write access

---
+++ src/ado-sessions.adb
@@ -0,0 +1,247 @@
+-----------------------------------------------------------------------
+--  ADO Sessions -- Sessions Management
+--  Copyright (C) 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Util.Log;
+with Util.Log.Loggers;
+with Ada.Unchecked_Deallocation;
+with ADO.Sequences;
+package body ADO.Sessions is
+
+   use Util.Log;
+
+   Log : constant Loggers.Logger := Loggers.Create ("ADO.Sessions");
+
+   procedure Check_Session (Database : in Session'Class);
+   pragma Inline (Check_Session);
+
+   procedure Check_Session (Database : in Session'Class) is
+   begin
+      if Database.Impl = null then
+         Log.Error ("Session is closed or not initialized");
+         raise NOT_OPEN;
+      end if;
+   end Check_Session;
+
+   --  ---------
+   --  Session
+   --  ---------
+
+   --  ------------------------------
+   --  Get the session status.
+   --  ------------------------------
+   function Get_Status (Database : in Session) return ADO.Databases.Connection_Status is
+   begin
+      if Database.Impl = null then
+         return ADO.Databases.CLOSED;
+      end if;
+      return Database.Impl.Database.Get_Status;
+   end Get_Status;
+
+   --  ------------------------------
+   --  Close the session.
+   --  ------------------------------
+   procedure Close (Database : in out Session) is
+
+      procedure Free is new
+        Ada.Unchecked_Deallocation (Object => Session_Record,
+                                    Name   => Session_Record_Access);
+
+   begin
+      Log.Info ("Closing session");
+      if Database.Impl /= null then
+         Database.Impl.Database.Close;
+         Database.Impl.Counter := Database.Impl.Counter - 1;
+         if Database.Impl.Counter = 0 then
+            Free (Database.Impl);
+         end if;
+         Database.Impl := null;
+      end if;
+   end Close;
+
+   --  ------------------------------
+   --  Get the database connection.
+   --  ------------------------------
+   function Get_Connection (Database : in Session) return ADO.Databases.Connection'Class is
+   begin
+      Check_Session (Database);
+      return Database.Impl.Database;
+   end Get_Connection;
+
+   --  ------------------------------
+   --  Attach the object to the session.
+   --  ------------------------------
+   procedure Attach (Database : in out Session;
+                     Object   : in ADO.Objects.Object_Ref'Class) is
+   begin
+      Check_Session (Database);
+   end Attach;
+
+   --  ------------------------------
+   --  Check if the session contains the object identified by the given key.
+   --  ------------------------------
+   function Contains (Database : in Session;
+                      Item     : in ADO.Objects.Object_Key) return Boolean is
+   begin
+      Check_Session (Database);
+      return ADO.Objects.Cache.Contains (Database.Impl.Cache, Item);
+   end Contains;
+
+   --  ------------------------------
+   --  Remove the object from the session cache.
+   --  ------------------------------
+   procedure Evict (Database : in out Session;
+                    Item     : in ADO.Objects.Object_Key) is
+   begin
+      Check_Session (Database);
+      ADO.Objects.Cache.Remove (Database.Impl.Cache, Item);
+   end Evict;
+
+   --  ------------------------------
+   --  Create a query statement.  The statement is not prepared
+   --  ------------------------------
+   function Create_Statement (Database : in Session;
+                              Table    : in ADO.Schemas.Class_Mapping_Access)
+                              return Query_Statement is
+   begin
+      Check_Session (Database);
+      return Database.Impl.Database.Create_Statement (Table);
+   end Create_Statement;
+
+   --  ------------------------------
+   --  Create a query statement.  The statement is not prepared
+   --  ------------------------------
+   function Create_Statement (Database : in Session;
+                              Query    : in String)
+                              return Query_Statement is
+   begin
+      Check_Session (Database);
+      return Database.Impl.Database.Create_Statement (Query);
+   end Create_Statement;
+
+   --  ---------
+   --  Master Session
+   --  ---------
+
+   --  ------------------------------
+   --  Start a transaction.
+   --  ------------------------------
+   procedure Begin_Transaction (Database : in out Master_Session) is
+   begin
+      Log.Info ("Begin transaction");
+
+      Check_Session (Database);
+      Database.Impl.Database.Begin_Transaction;
+   end Begin_Transaction;
+
+   --  ------------------------------
+   --  Commit the current transaction.
+   --  ------------------------------
+   procedure Commit (Database : in out Master_Session) is
+   begin
+      Log.Info ("Commit transaction");
+
+      Check_Session (Database);
+      Database.Impl.Database.Commit;
+   end Commit;
+
+   --  ------------------------------
+   --  Rollback the current transaction.
+   --  ------------------------------
+   procedure Rollback (Database : in out Master_Session) is
+   begin
+      Log.Info ("Rollback transaction");
+
+      Check_Session (Database);
+      Database.Impl.Database.Rollback;
+   end Rollback;
+
+   --  ------------------------------
+   --  Allocate an identifier for the table.
+   --  ------------------------------
+   procedure Allocate (Database : in out Master_Session;
+                       Name     : in String;
+                       Id       : out Identifier) is
+   begin
+      Log.Info ("Allocate id for {0}", Name);
+
+      Check_Session (Database);
+      ADO.Sequences.Allocate (Database.Sequences.all, Name, Id);
+   end Allocate;
+
+   --  ------------------------------
+   --  Flush the objects that were modified.
+   --  ------------------------------
+   procedure Flush (Database : in out Master_Session) is
+   begin
+      Check_Session (Database);
+   end Flush;
+
+   overriding
+   procedure Adjust (Object : in out Session) is
+   begin
+      if Object.Impl /= null then
+         Object.Impl.Counter := Object.Impl.Counter + 1;
+      end if;
+   end Adjust;
+
+   overriding
+   procedure Finalize (Object : in out Session) is
+   begin
+      if Object.Impl /= null then
+         if Object.Impl.Counter = 1 then
+            Object.Close;
+         else
+            Object.Impl.Counter := Object.Impl.Counter - 1;
+         end if;
+      end if;
+   end Finalize;
+
+   --  ------------------------------
+   --  Create a delete statement
+   --  ------------------------------
+   function Create_Statement (Database : in Master_Session;
+                              Table    : in ADO.Schemas.Class_Mapping_Access)
+                              return Delete_Statement is
+   begin
+      Check_Session (Database);
+      return Database.Impl.Database.Create_Statement (Table);
+   end Create_Statement;
+
+   --  ------------------------------
+   --  Create an update statement
+   --  ------------------------------
+   function Create_Statement (Database : in Master_Session;
+                              Table    : in ADO.Schemas.Class_Mapping_Access)
+                              return Update_Statement is
+   begin
+      Check_Session (Database);
+      return Database.Impl.Database.Create_Statement (Table);
+   end Create_Statement;
+
+   --  ------------------------------
+   --  Create an insert statement
+   --  ------------------------------
+   function Create_Statement (Database : in Master_Session;
+                              Table    : in ADO.Schemas.Class_Mapping_Access)
+                              return Insert_Statement is
+   begin
+      Check_Session (Database);
+      return Database.Impl.Database.Create_Statement (Table);
+   end Create_Statement;
+
+end ADO.Sessions;
---
+++ src/ado-sessions.ads
@@ -0,0 +1,163 @@
+-----------------------------------------------------------------------
+--  ADO Sessions -- Sessions Management
+--  Copyright (C) 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Ada.Finalization;
+
+with ADO.Schemas;
+with ADO.Statements;
+with ADO.Objects;
+with ADO.Objects.Cache;
+with ADO.Databases;
+with Util.Concurrent.Counters;
+limited with ADO.Sequences;
+
+package ADO.Sessions is
+
+   use ADO.Statements;
+
+   --  Raised for all errors reported by the database
+   DB_Error : exception;
+
+   --  Raised if the database connection is not open.
+   NOT_OPEN : exception;
+
+   NOT_FOUND : exception;
+
+   NO_DATABASE : exception;
+
+   --  Raised when the connection URI is invalid.
+   Connection_Error : exception;
+
+   type Object_Factory is tagged private;
+
+   --  ---------
+   --  Session
+   --  ---------
+   --  Read-only database connection (or slave connection).
+   --
+   type Session is tagged private;
+
+   --  Get the session status.
+   function Get_Status (Database : in Session) return ADO.Databases.Connection_Status;
+
+   --  Close the session.
+   procedure Close (Database : in out Session);
+
+   --  Get the database connection.
+   function Get_Connection (Database : in Session) return ADO.Databases.Connection'Class;
+
+   --  Attach the object to the session.
+   procedure Attach (Database : in out Session;
+                     Object   : in ADO.Objects.Object_Ref'Class);
+
+   --  Check if the session contains the object identified by the given key.
+   function Contains (Database : in Session;
+                      Item     : in ADO.Objects.Object_Key) return Boolean;
+
+   --  Remove the object from the session cache.
+   procedure Evict (Database : in out Session;
+                    Item     : in ADO.Objects.Object_Key);
+
+   --  Create a query statement.  The statement is not prepared
+   function Create_Statement (Database : in Session;
+                              Table    : in ADO.Schemas.Class_Mapping_Access)
+                              return Query_Statement;
+
+   --  Create a query statement.  The statement is not prepared
+   function Create_Statement (Database : in Session;
+                              Query    : in String)
+                              return Query_Statement;
+
+   --  ---------
+   --  Master Session
+   --  ---------
+   --  Read-write session.
+   --
+   type Master_Session is new Session with private;
+
+   --  Start a transaction.
+   procedure Begin_Transaction (Database : in out Master_Session);
+
+   --  Commit the current transaction.
+   procedure Commit (Database : in out Master_Session);
+
+   --  Rollback the current transaction.
+   procedure Rollback (Database : in out Master_Session);
+
+   --  Allocate an identifier for the table.
+   procedure Allocate (Database : in out Master_Session;
+                       Name     : in String;
+                       Id       : out Identifier);
+
+   --  Flush the objects that were modified.
+   procedure Flush (Database : in out Master_Session);
+
+   --  Create a delete statement
+   function Create_Statement (Database : in Master_Session;
+                              Table    : in ADO.Schemas.Class_Mapping_Access)
+                              return Delete_Statement;
+
+   --  Create an update statement
+   function Create_Statement (Database : in Master_Session;
+                              Table    : in ADO.Schemas.Class_Mapping_Access)
+                              return Update_Statement;
+
+   --  Create an insert statement
+   function Create_Statement (Database : in Master_Session;
+                              Table    : in ADO.Schemas.Class_Mapping_Access)
+                              return Insert_Statement;
+
+private
+
+   type Session_Record;
+   type Session_Record_Access is access all Session_Record;
+
+   type Object_Factory is tagged record
+      A : Integer;
+   end record;
+   type Object_Factory_Access is access all Object_Factory'Class;
+
+   type Session_Proxy is record
+      Counter : Util.Concurrent.Counters.Counter;
+      Session : Session_Record_Access;
+      Factory : Object_Factory_Access;
+   end record;
+   type Session_Proxy_Access is access all Session_Proxy;
+
+   type Session_Record is record
+      Counter  : Natural := 1;
+      Database : ADO.Databases.Master_Connection;
+      Proxy    : Session_Proxy_Access;
+      Cache    : ADO.Objects.Cache.Object_Cache;
+   end record;
+
+   type Session is new Ada.Finalization.Controlled with record
+      Impl : Session_Record_Access := null;
+   end record;
+
+   overriding
+   procedure Adjust (Object : in out Session);
+
+   overriding
+   procedure Finalize (Object : in out Session);
+
+   type Master_Session is new Session with record
+      Sequences : access ADO.Sequences.Factory;
+   end record;
+
+end ADO.Sessions;
