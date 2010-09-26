40b04fdf409e71a47623aed6e3d16194ab9f31c6
Stephane Carrez <Stephane.Carrez@gmail.com>
2010-09-26 15:29:24 +0000

    Definition and implementation of list of parameters for SQL queries

---
+++ src/ado-parameters.adb
@@ -0,0 +1,379 @@
+-----------------------------------------------------------------------
+--  ADO Parameters -- Parameters for queries
+--  Copyright (C) 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+--  with GNAT.Calendar.Time_IO;      use GNAT.Calendar.Time_IO;
+with Util.Log;
+with Util.Log.Loggers;
+with Ada.Calendar.Formatting;
+package body ADO.Parameters is
+
+   use Util.Log;
+
+   use Parameter_Vectors;
+
+   Log : constant Loggers.Logger := Loggers.Create ("ADO.Statements");
+
+   --
+   function "+"(Str : in String)
+                     return Unbounded_String renames To_Unbounded_String;
+
+   procedure Bind_Param (Params : in out Abstract_List;
+                         Name   : in String;
+                         Value  : in Boolean) is
+      Param : constant Parameter := Parameter '(T        => T_Boolean,
+                                                Name     => + (Name),
+                                                Position => 0,
+                                                Bool     => Value);
+   begin
+      Abstract_List'Class (Params).Add_Parameter (Param);
+   end Bind_Param;
+
+   procedure Bind_Param (Params : in out Abstract_List;
+                         Name   : in String;
+                         Value  : in Integer) is
+      Param : constant Parameter := Parameter '(T        => T_INTEGER,
+                                                Name     => +(Name),
+                                                Position => 0,
+                                                Num      => Value);
+   begin
+      Abstract_List'Class (Params).Add_Parameter (Param);
+   end Bind_Param;
+
+   procedure Bind_Param (Params : in out Abstract_List;
+                         Name   : in String;
+                         Value  : in Long_Integer) is
+      Param : constant Parameter := Parameter '(T        => T_LONG_INTEGER,
+                                                Name     => +(Name),
+                                                Position => 0,
+                                                Long_Num => Value);
+   begin
+      Abstract_List'Class (Params).Add_Parameter (Param);
+   end Bind_Param;
+
+   procedure Bind_Param (Params : in out Abstract_List;
+                         Name  : in String;
+                         Value : in Identifier) is
+      Param : constant Parameter := Parameter '(T        => T_LONG_INTEGER,
+                                                Name     => +(Name),
+                                                Position => 0,
+                                                Long_Num => Long_Integer (Value));
+   begin
+      Abstract_List'Class (Params).Add_Parameter (Param);
+   end Bind_Param;
+
+   procedure Bind_Param (Params : in out Abstract_List;
+                         Name  : in String;
+                         Value : in String) is
+   begin
+      Abstract_List'Class (Params).
+	Add_Parameter (Parameter '(T    => T_STRING,
+				   Name => To_Unbounded_String (Name),
+				   Position => 0,
+				   Str      => To_Unbounded_String (Value)));
+   end Bind_Param;
+
+   procedure Bind_Param (Params : in out Abstract_List;
+                         Name  : in String;
+                         Value : in Unbounded_String) is
+      Param : constant Parameter := Parameter '(T => T_STRING, Name => +(Name),
+                                                Position => 0, Str => Value);
+   begin
+      Abstract_List'Class (Params).Add_Parameter (Param);
+   end Bind_Param;
+
+   procedure Bind_Param (Params : in out Abstract_List;
+                         Name   : in String;
+                         Value  : in Ada.Calendar.Time) is
+      Param : constant Parameter := Parameter '(T => T_DATE, Name => +(Name),
+                                                Position => 0, Time => Value);
+   begin
+      Abstract_List'Class (Params).Add_Parameter (Param);
+   end Bind_Param;
+
+   procedure Bind_Param (Params   : in out Abstract_List;
+                         Position : in Natural;
+                         Value    : in Boolean) is
+      P : Parameter := Parameter '(T        => T_BOOLEAN,
+                                   Name     => Null_Unbounded_String,
+                                   Position => Position,
+                                   Bool     => Value);
+   begin
+      if Position = 0 then
+         P.Position := Abstract_List'Class (Params).Length + 1;
+      end if;
+      Abstract_List'Class (Params).Add_Parameter (P);
+   end Bind_Param;
+
+   procedure Bind_Param (Params   : in out Abstract_List;
+                         Position : in Natural;
+                         Value    : in Long_Integer) is
+      P : Parameter := Parameter '(T        => T_LONG_INTEGER,
+                                   Name     => Null_Unbounded_String,
+                                   Position => Position,
+                                   Long_Num => Value);
+   begin
+      if Position = 0 then
+         P.Position := Abstract_List'Class (Params).Length + 1;
+      end if;
+      Abstract_List'Class (Params).Add_Parameter (P);
+   end Bind_Param;
+
+   procedure Bind_Param (Params   : in out Abstract_List;
+                         Position : in Natural;
+                         Value    : in Identifier) is
+      P : Parameter := Parameter '(T        => T_LONG_INTEGER,
+                                   Name     => Null_Unbounded_String,
+                                   Position => Position,
+                                   Long_Num => Long_Integer (Value));
+   begin
+      if Position = 0 then
+         P.Position := Abstract_List'Class (Params).Length + 1;
+      end if;
+      Abstract_List'Class (Params).Add_Parameter (P);
+   end Bind_Param;
+
+   procedure Bind_Param (Params   : in out Abstract_List;
+                         Position : in Natural;
+                         Value    : in Integer) is
+      P : Parameter := Parameter '(T        => T_INTEGER,
+                                   Name     => Null_Unbounded_String,
+                                   Position => Position,
+                                   Num      => Value);
+   begin
+      if Position = 0 then
+         P.Position := Abstract_List'Class (Params).Length + 1;
+      end if;
+      Abstract_List'Class (Params).Add_Parameter (P);
+   end Bind_Param;
+
+   procedure Bind_Param (Params   : in out Abstract_List;
+                         Position : in Natural;
+                         Value    : in String) is
+      P : Parameter := Parameter '(T        => T_STRING,
+                                   Name     => Null_Unbounded_String,
+                                   Position => Position,
+                                   Str      => +(Value));
+   begin
+      if Position = 0 then
+         P.Position := Abstract_List'Class (Params).Length + 1;
+      end if;
+      Abstract_List'Class (Params).Add_Parameter (P);
+   end Bind_Param;
+
+   procedure Bind_Param (Params   : in out Abstract_List;
+                         Position : in Natural;
+                         Value    : in Unbounded_String) is
+      P : Parameter := Parameter '(T        => T_STRING,
+                                   Name     => Null_Unbounded_String,
+                                   Position => Position,
+                                   Str      => Value);
+   begin
+      if Position = 0 then
+         P.Position := Abstract_List'Class (Params).Length + 1;
+      end if;
+      Abstract_List'Class (Params).Add_Parameter (P);
+   end Bind_Param;
+
+   procedure Bind_Param (Params    : in out Abstract_List;
+                         Position : in Natural;
+                         Value    : in Ada.Calendar.Time) is
+      P : Parameter := Parameter '(T        => T_DATE,
+                                   Name     => Null_Unbounded_String,
+                                   Position => Position,
+                                   Time     => Value);
+   begin
+      if Position = 0 then
+         P.Position := Abstract_List'Class (Params).Length + 1;
+      end if;
+      Abstract_List'Class (Params).Add_Parameter (P);
+   end Bind_Param;
+
+   procedure Add_Param (Params : in out Abstract_List;
+                        Value  : in Boolean) is
+   begin
+      Params.Bind_Param (Position => 0, Value => Value);
+   end Add_Param;
+
+   procedure Add_Param (Params : in out Abstract_List;
+                        Value : in Long_Integer) is
+   begin
+      Params.Bind_Param (Position => 0, Value => Value);
+   end Add_Param;
+
+   procedure Add_Param (Params : in out Abstract_List;
+                        Value : in Identifier) is
+   begin
+      Params.Bind_Param (Position => 0, Value => Value);
+   end Add_Param;
+
+   procedure Add_Param (Params : in out Abstract_List;
+                        Value : in Integer) is
+   begin
+      Params.Bind_Param (Position => 0, Value => Value);
+   end Add_Param;
+
+   procedure Add_Param (Params : in out Abstract_List;
+                        Value : in String) is
+   begin
+      Params.Bind_Param (Position => 0, Value => Value);
+   end Add_Param;
+
+   procedure Add_Param (Params : in out Abstract_List;
+                        Value : in Unbounded_String) is
+   begin
+      Params.Bind_Param (Position => 0, Value => Value);
+   end Add_Param;
+
+   procedure Add_Param (Params : in out Abstract_List;
+                        Value : in Ada.Calendar.Time) is
+   begin
+      Params.Bind_Param (Position => 0, Value => Value);
+   end Add_Param;
+
+   --  Expand the query parameters
+   function Expand (Params : in Abstract_List;
+                    SQL    : in String) return String is
+
+      procedure Escape_Sql (Buffer : in out Unbounded_String;
+                            Item   : in String) is
+         C  : Character;
+      begin
+         for I in Item'Range loop
+            C := Item (I);
+            if C = '\' or C = ASCII.CR or C = ''' or C = '"' then
+               Append (Buffer, '\');
+            end if;
+            Append (Buffer, C);
+         end loop;
+      end Escape_Sql;
+
+      procedure Append_Date (Buffer : in out Unbounded_String;
+                             Time   : in Ada.Calendar.Time) is
+      begin
+         Append (Buffer, "'");
+         Append (Buffer,
+                 Ada.Calendar.Formatting.Image (Time, True));
+         Append (Buffer, "'");
+      end Append_Date;
+
+      Num    : Natural := 0;
+      Pos    : Natural;
+      C      : Character;
+      Buffer : Unbounded_String;
+
+      use ADO.Parameters;
+
+   begin
+      --  Build the SQL query by injecting the parameters.
+      Pos := Sql'First;
+      while Pos <= Sql'Last loop
+         C := Sql (Pos);
+         if C = '\' then
+            Append (Buffer, C);
+            Pos := Pos + 1;
+            exit when Pos > Sql'Last;
+            Append (Buffer, Sql (Pos));
+
+         elsif C = ':' or C = '?' then
+            if C = ':' then
+               Pos := Pos + 1;
+               exit when Pos > Sql'Last;
+               C := Sql (Pos);
+               Num := Natural (Character'Pos (C) - Character'Pos ('0'));
+            else
+               Num := Num + 1;
+            end if;
+            declare
+               P   : constant Parameter := Abstract_List'Class (Params).Element (Num);
+            begin
+               case P.T is
+                  when T_LONG_INTEGER =>
+                     Append (Buffer, Long_Integer'Image (P.Long_Num));
+
+                  when T_INTEGER =>
+                     Append (Buffer, Integer'Image (P.Num));
+
+                  when T_BOOLEAN =>
+                     if P.Bool then
+                        Append (Buffer, "1");
+                     else
+                        Append (Buffer, "0");
+                     end if;
+
+                  when T_DATE =>
+                     Append_Date (Buffer, P.Time);
+
+                  when others =>
+                     Append (Buffer, "'");
+                     Escape_Sql (Buffer, To_String (P.Str));
+                     Append (Buffer, "'");
+               end case;
+            end;
+         else
+            Append (Buffer, C);
+         end if;
+         Pos := Pos + 1;
+      end loop;
+      return To_String (Buffer);
+   end Expand;
+
+   --  ------------------------------
+   --  Add the parameter in the list.
+   --  ------------------------------
+   procedure Add_Parameter (Params : in out List;
+			    Param  : in Parameter) is
+   begin
+      Params.Params.Append (Param);
+   end Add_Parameter;
+
+   --  ------------------------------
+   --  Return the number of parameters in the list.
+   --  ------------------------------
+   function Length (Params : in List) return Natural is
+   begin
+      return Natural (Length (Params.Params));
+   end Length;
+
+   --  ------------------------------
+   --  Clear the list of parameters.
+   --  ------------------------------
+   procedure Clear (Params : in out List) is
+   begin
+      Parameter_Vectors.Clear (Params.Params);
+   end Clear;
+
+   --  ------------------------------
+   --  Set the parameters from another parameter list.
+   --  ------------------------------
+   procedure Set_Parameters (Params : in out List;
+                             From   : in Abstract_List'Class) is
+      L : constant List'Class := List'Class (From);
+   begin
+      Params.Params := L.Params;
+   end Set_Parameters;
+
+   --  ------------------------------
+   --  Return the parameter at the given position
+   --  ------------------------------
+   function Element (Params   : in List;
+                     Position : in Natural) return Parameter is
+   begin
+      return Element (Params.Params, Position);
+   end Element;
+
+end ADO.Parameters;
---
+++ src/ado-parameters.ads
@@ -0,0 +1,172 @@
+-----------------------------------------------------------------------
+--  ADO Parameters -- Parameters for queries
+--  Copyright (C) 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Ada.Strings.Unbounded;
+with Ada.Finalization;
+with Ada.Calendar;
+with Ada.Containers.Indefinite_Vectors;
+
+--  Defines a list of parameters for an SQL statement.
+--
+package ADO.Parameters is
+
+   use Ada.Strings.Unbounded;
+
+   type Parameter_Type is (T_STRING, T_DATE, T_LONG_INTEGER, T_INTEGER, T_Boolean);
+
+   type Parameter (T : Parameter_Type) is record
+      Name     : Unbounded_String;
+      Position : Natural := 0;
+      case T is
+         when T_LONG_INTEGER =>
+            Long_Num : Long_Integer;
+
+         when T_INTEGER =>
+            Num : Integer;
+
+         when T_BOOLEAN =>
+            Bool : Boolean;
+
+         when T_DATE =>
+            Time : Ada.Calendar.Time;
+
+         when others =>
+            Str : Unbounded_String;
+      end case;
+   end record;
+
+   type Abstract_List is abstract new Ada.Finalization.Controlled with private;
+   type Abstract_List_Access is access all Abstract_List'Class;
+
+   --  Add the parameter in the list.
+   procedure Add_Parameter (Params : in out Abstract_List;
+                            Param  : in Parameter) is abstract;
+
+   --  Set the parameters from another parameter list.
+   procedure Set_Parameters (Parameters : in out Abstract_List;
+                             From       : in Abstract_List'Class) is abstract;
+
+   --  Return the number of parameters in the list.
+   function Length (Params : in Abstract_List) return Natural is abstract;
+
+   --  Return the parameter at the given position
+   function Element (Params   : in Abstract_List;
+                     Position : in Natural) return Parameter is abstract;
+
+   --  Clear the list of parameters.
+   procedure Clear (Parameters : in out Abstract_List) is abstract;
+
+   --  Operations to bind a parameter
+   procedure Bind_Param (Params : in out Abstract_List;
+                         Name   : in String;
+                         Value  : in Boolean);
+   procedure Bind_Param (Params : in out Abstract_List;
+                         Name   : in String;
+                         Value  : in Long_Integer);
+   procedure Bind_Param (Params : in out Abstract_List;
+                         Name   : in String;
+                         Value  : in Identifier);
+   procedure Bind_Param (Params : in out Abstract_List;
+                         Name   : in String;
+                         Value  : in Integer);
+   procedure Bind_Param (Params : in out Abstract_List;
+                         Name   : in String;
+                         Value  : in String);
+   procedure Bind_Param (Params : in out Abstract_List;
+                         Name   : in String;
+                         Value  : in Unbounded_String);
+   procedure Bind_Param (Params : in out Abstract_List;
+                         Name   : in String;
+                         Value  : in Ada.Calendar.Time);
+
+   procedure Bind_Param (Params   : in out Abstract_List;
+                         Position : in Natural;
+                         Value    : in Boolean);
+   procedure Bind_Param (Params   : in out Abstract_List;
+                         Position : in Natural;
+                         Value    : in Long_Integer);
+   procedure Bind_Param (Params   : in out Abstract_List;
+                         Position : in Natural;
+                         Value    : in Identifier);
+   procedure Bind_Param (Params   : in out Abstract_List;
+                         Position : in Natural;
+                         Value    : in Integer);
+   procedure Bind_Param (Params   : in out Abstract_List;
+                         Position : in Natural;
+                         Value    : in String);
+   procedure Bind_Param (Params   : in out Abstract_List;
+                         Position : in Natural;
+                         Value    : in Unbounded_String);
+   procedure Bind_Param (Params   : in out Abstract_List;
+                         Position : in Natural;
+                         Value    : in Ada.Calendar.Time);
+
+   procedure Add_Param (Params : in out Abstract_List;
+                         Value : in Boolean);
+   procedure Add_Param (Params : in out Abstract_List;
+                         Value : in Long_Integer);
+   procedure Add_Param (Params : in out Abstract_List;
+                         Value : in Identifier);
+   procedure Add_Param (Params : in out Abstract_List;
+                         Value : in Integer);
+   procedure Add_Param (Params : in out Abstract_List;
+                         Value : in String);
+   procedure Add_Param (Params : in out Abstract_List;
+                         Value : in Unbounded_String);
+   procedure Add_Param (Params : in out Abstract_List;
+                         Value : in Ada.Calendar.Time);
+
+   --  Expand the query parameters
+   function Expand (Params : in Abstract_List;
+                    SQL    : in String) return String;
+
+   --  ------------------------------
+   --  List of parameters
+   --  ------------------------------
+   --  The <b>List</b> is an implementation of the parameter list.
+   type List is new Abstract_List with private;
+
+   procedure Add_Parameter (Params : in out List;
+                            Param  : in Parameter);
+
+   procedure Set_Parameters (Params : in out List;
+                             From   : in Abstract_List'Class);
+
+   --  Return the number of parameters in the list.
+   function Length (Params : in List) return Natural;
+
+   --  Return the parameter at the given position
+   function Element (Params   : in List;
+                     Position : in Natural) return Parameter;
+
+   --  Clear the list of parameters.
+   procedure Clear (Params : in out List);
+
+private
+
+   package Parameter_Vectors is
+     new Ada.Containers.Indefinite_Vectors (Index_Type   => Positive,
+                                            Element_Type => Parameter);
+
+   type Abstract_List is abstract new Ada.Finalization.Controlled with null record;
+
+   type List is new Abstract_List with record
+      Params : Parameter_Vectors.Vector;
+   end record;
+
+end ADO.Parameters;
