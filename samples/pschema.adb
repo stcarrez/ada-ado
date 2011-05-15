3b7d00db1ff0cb6d0dcf6583b950617b22ebf428
Stephane Carrez <Stephane.Carrez@gmail.com>
2011-05-15 16:06:50 +0000

    Example to read the database schema

---
+++ samples/pschema.adb
@@ -0,0 +1,90 @@
+-----------------------------------------------------------------------
+--  pschema - Print the database schema
+--  Copyright (C) 2009, 2010, 2011 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with ADO;
+with ADO.Drivers.Mysql;
+with ADO.Schemas.Mysql;
+with ADO.Databases;
+with ADO.Sessions;
+with ADO.Sessions.Factory;
+with ADO.Schemas;
+
+with Ada.Text_IO;
+with Ada.Strings.Unbounded;
+with Ada.Command_Line;
+
+with Util.Log.Loggers;
+
+procedure Pschema is
+
+   use ADO;
+   use Ada;
+   use Ada.Strings.Unbounded;
+   use ADO.Schemas;
+
+   Controller : ADO.Databases.DataSource;
+   Factory    : ADO.Sessions.Factory.Session_Factory;
+
+   Verbose : Boolean;
+
+begin
+   Util.Log.Loggers.Initialize ("samples.properties");
+
+   --  Initialize the database drivers.
+   ADO.Drivers.Initialize ("samples.properties");
+
+   --  Initialize the session factory to connect to the
+   --  database defined by 'ado.database' property.
+   Factory.Create (ADO.Drivers.Get_Config ("ado.database"));
+
+   declare
+      DB     : ADO.Sessions.Master_Session := Factory.Get_Master_Session;
+      Schema : ADO.Schemas.Schema_Definition;
+      Iter   : Table_Cursor;
+   begin
+      ADO.Schemas.Mysql.Load_Schema (DB.Get_Connection, Schema);
+
+      --  Dump the database schema using SQL create table forms.
+      Iter := Get_Tables (Schema);
+      while Has_Element (Iter) loop
+         declare
+            Table : constant Table_Definition := Element (Iter);
+            CITer : Column_Cursor := Get_Columns (Table);
+         begin
+            Ada.Text_IO.Put_Line ("create table " & Get_Name (Table) & " (");
+            while Has_Element (CIter) loop
+               declare
+                  Col : constant Column_Definition := Element (CIter);
+               begin
+                  Ada.Text_IO.Put ("  ");
+                  Ada.Text_IO.Put (Get_Name (Col));
+                  Ada.Text_IO.Put (" ");
+                  Ada.Text_IO.Put (Column_Type'Image (Get_Type (Col)));
+                  if not Is_Null (Col) then
+                     Ada.Text_IO.Put (" not null");
+                  end if;
+                  Ada.Text_IO.Put_Line (",");
+               end;
+               Next (CIter);
+            end loop;
+            Ada.Text_IO.Put_Line (");");
+         end;
+         Next (Iter);
+      end loop;
+   end;
+end Pschema;
