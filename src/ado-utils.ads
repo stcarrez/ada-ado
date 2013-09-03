3a4aed2caf558b1d893ff221b69da35f227e4a49
Stephane Carrez <Stephane.Carrez@gmail.com>
2013-09-03 19:14:38 +0000

    New function To_Identifier and To_Object to help in converting an Object into an identifier

---
+++ src/ado-utils.ads
@@ -0,0 +1,28 @@
+-----------------------------------------------------------------------
+--  ado-utils -- Utility operations for ADO
+--  Copyright (C) 2013 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Util.Beans.Objects;
+package ADO.Utils is
+
+   --  Build a bean object from the identifier.
+   function To_Object (Id : in ADO.Identifier) return Util.Beans.Objects.Object;
+
+   --  Build the identifier from the bean object.
+   function To_Identifier (Value : in Util.Beans.Objects.Object) return ADO.Identifier;
+
+end ADO.Utils;
