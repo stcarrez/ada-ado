1c6dcc4d138ba6a1870b3acbebeac1a1963942ed
Stephane Carrez <Stephane.Carrez@gmail.com>
2015-11-17 21:36:45 +0100

    Declare Test_Set_Query and Test_Set_Limit procedures

--- regtests/ado-queries-tests.ads
+++ regtests/ado-queries-tests.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  ado-queries-tests -- Test loading of database queries
---  Copyright (C) 2011, 2012 Stephane Carrez
+--  Copyright (C) 2011, 2012, 2015 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -27,5 +27,11 @@ package ADO.Queries.Tests is
 
    --  Test the Initialize operation called several times
    procedure Test_Initialize (T : in out Test);
+
+   --  Test the Set_Query operation.
+   procedure Test_Set_Query (T : in out Test);
+
+   --  Test the Set_Limit operation.
+   procedure Test_Set_Limit (T : in out Test);
 
 end ADO.Queries.Tests;
