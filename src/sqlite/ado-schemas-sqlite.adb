3977c8482e727be71bb44b74b1413db70d14bc9d
ciceron <Stephane.Carrez@gmail.com>
2015-11-18 23:47:03 +0100

    Implement the Load_Schema procedure for SQLite

---
+++ src/sqlite/ado-schemas-sqlite.adb
@@ -0,0 +1,133 @@
+-----------------------------------------------------------------------
+--  ado.schemas -- Database Schemas
+--  Copyright (C) 2015 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with ADO.Statements;
+with ADO.Statements.Create;
+with Ada.Strings.Fixed;
+with Util.Strings.Transforms;
+package body ADO.Schemas.Sqlite is
+
+   use ADO.Statements;
+
+   procedure Load_Table_Schema (C : in ADO.Drivers.Connections.Database_Connection'Class;
+                                Table : in Table_Definition);
+
+   function String_To_Type (Value : in String) return Column_Type;
+
+   function String_To_Type (Value : in String) return Column_Type is
+      Pos : Natural;
+   begin
+      if Value = "date" then
+         return T_DATE;
+      elsif Value = "datetime" then
+         return T_DATE_TIME;
+      elsif Value = "int" or Value = "integer" then
+         return T_INTEGER;
+      elsif Value = "bigint" then
+         return T_LONG_INTEGER;
+      elsif Value = "tinyint" then
+         return T_TINYINT;
+      elsif Value = "smallint" then
+         return T_SMALLINT;
+      elsif Value = "blob" then
+         return T_BLOB;
+      end if;
+      Pos := Ada.Strings.Fixed.Index (Value, "(");
+      if Pos > 0 then
+         declare
+            Name : constant String := Value (Value'First .. Pos - 1);
+         begin
+            if Name = "varchar" then
+               return T_VARCHAR;
+            elsif Name = "real" or Name = "float" or Name = "double" then
+               return T_FLOAT;
+            elsif Name = "int" then
+               return T_INTEGER;
+            elsif Name = "bigint" then
+               return T_LONG_INTEGER;
+            else
+               return T_UNKNOWN;
+            end if;
+         end;
+      end if;
+      return T_UNKNOWN;
+   end String_To_Type;
+
+   --  ------------------------------
+   --  Load the table definition
+   --  ------------------------------
+   procedure Load_Table_Schema (C : in ADO.Drivers.Connections.Database_Connection'Class;
+                                Table : in Table_Definition) is
+      Name : constant String := Get_Name (Table);
+      Stmt : Query_Statement
+        := Create.Create_Statement (C.Create_Statement ("pragma table_info('" & Name & "')"));
+      Last : Column_Definition := null;
+      Col  : Column_Definition;
+      Value : Unbounded_String;
+   begin
+      Stmt.Execute;
+      while Stmt.Has_Elements loop
+         Col           := new Column;
+         Col.Name      := Stmt.Get_Unbounded_String (1);
+         Col.Collation := Stmt.Get_Unbounded_String (2);
+         Col.Default   := Stmt.Get_Unbounded_String (5);
+         if Last /= null then
+            Last.Next_Column := Col;
+         else
+            Table.First_Column := Col;
+         end if;
+
+         Value := Stmt.Get_Unbounded_String (2);
+         Col.Col_Type
+           := String_To_Type (Util.Strings.Transforms.To_Lower_Case (To_String (Value)));
+
+         Value := Stmt.Get_Unbounded_String (3);
+         Col.Is_Null := Value = "0";
+
+         Last := Col;
+         Stmt.Next;
+      end loop;
+   end Load_Table_Schema;
+
+   --  ------------------------------
+   --  Load the database schema
+   --  ------------------------------
+   procedure Load_Schema (C      : in ADO.Drivers.Connections.Database_Connection'Class;
+                          Schema : out Schema_Definition) is
+      Stmt  : Query_Statement
+        := Create.Create_Statement (C.Create_Statement ("SELECT NAME FROM sqlite_master"));
+      Table : Table_Definition;
+      Last  : Table_Definition := null;
+   begin
+      Schema.Schema := new ADO.Schemas.Schema;
+      Stmt.Execute;
+
+      while Stmt.Has_Elements loop
+         Table      := new ADO.Schemas.Table;
+         Table.Name := Stmt.Get_Unbounded_String (0);
+         if Last /= null then
+            Last.Next_Table := Table;
+         else
+            Schema.Schema.First_Table := Table;
+         end if;
+         Load_Table_Schema (C, Table);
+         Last := Table;
+         Stmt.Next;
+      end loop;
+   end Load_Schema;
+
+end ADO.Schemas.Sqlite;
