8b598fdc7534734f5406b02be10a9da750b834c9
Stephane Carrez <Stephane.Carrez@gmail.com>
2015-12-23 16:54:28 +0100

    Fix the schema unit test

--- regtests/ado-schemas-tests.adb
+++ regtests/ado-schemas-tests.adb
@@ -198,7 +198,7 @@ package body ADO.Schemas.Tests is
             Util.Tests.Assert_Equals (T, 15, Count,
                                       "Invalid number of tables found in the schema");
          elsif Driver = "mysql" then
-            Util.Tests.Assert_Equals (T, 9, Count,
+            Util.Tests.Assert_Equals (T, 8, Count,
                                       "Invalid number of tables found in the schema");
          end if;
       end;
