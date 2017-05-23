243843e626d612b762c76b10340168dc2cacd0c0
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-05-23 21:33:19 +0200

    Add a Expander_Access parameter to each Create_Statement operation

--- src/ado-statements-create.ads
+++ src/ado-statements-create.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  ADO Statements -- Database statements
---  Copyright (C) 2009, 2010, 2011 Stephane Carrez
+--  Copyright (C) 2009, 2010, 2011, 2017 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,15 +19,19 @@
 package ADO.Statements.Create is
 
    --  Create the query statement
-   function Create_Statement (Proxy : Query_Statement_Access) return Query_Statement;
+   function Create_Statement (Proxy    : in Query_Statement_Access;
+                              Expander : in ADO.Parameters.Expander_Access := null) return Query_Statement;
 
    --  Create the delete statement
-   function Create_Statement (Proxy : Delete_Statement_Access) return Delete_Statement;
+   function Create_Statement (Proxy    : in Delete_Statement_Access;
+                              Expander : in ADO.Parameters.Expander_Access := null) return Delete_Statement;
 
    --  Create an update statement
-   function Create_Statement (Proxy : Update_Statement_Access) return Update_Statement;
+   function Create_Statement (Proxy    : in Update_Statement_Access;
+                              Expander : in ADO.Parameters.Expander_Access := null) return Update_Statement;
 
    --  Create the insert statement.
-   function Create_Statement (Proxy : Update_Statement_Access) return Insert_Statement;
+   function Create_Statement (Proxy    : in Update_Statement_Access;
+                              Expander : in ADO.Parameters.Expander_Access := null) return Insert_Statement;
 
 end ADO.Statements.Create;
