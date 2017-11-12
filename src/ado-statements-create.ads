144a6232489abdb280149f16e8482579e8b75968
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-11-12 20:39:32 +0100

    Fix style compilation warning

--- src/ado-statements-create.ads
+++ src/ado-statements-create.ads
@@ -20,18 +20,22 @@ package ADO.Statements.Create is
 
    --  Create the query statement
    function Create_Statement (Proxy    : in Query_Statement_Access;
-                              Expander : in ADO.Parameters.Expander_Access := null) return Query_Statement;
+                              Expander : in ADO.Parameters.Expander_Access := null)
+                              return Query_Statement;
 
    --  Create the delete statement
    function Create_Statement (Proxy    : in Delete_Statement_Access;
-                              Expander : in ADO.Parameters.Expander_Access := null) return Delete_Statement;
+                              Expander : in ADO.Parameters.Expander_Access := null)
+                              return Delete_Statement;
 
    --  Create an update statement
    function Create_Statement (Proxy    : in Update_Statement_Access;
-                              Expander : in ADO.Parameters.Expander_Access := null) return Update_Statement;
+                              Expander : in ADO.Parameters.Expander_Access := null)
+                              return Update_Statement;
 
    --  Create the insert statement.
    function Create_Statement (Proxy    : in Update_Statement_Access;
-                              Expander : in ADO.Parameters.Expander_Access := null) return Insert_Statement;
+                              Expander : in ADO.Parameters.Expander_Access := null)
+                              return Insert_Statement;
 
 end ADO.Statements.Create;
