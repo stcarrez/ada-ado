a8bedf8e62b28da1a5df17c16ac3e5b580343a90
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-06-10 18:49:15 +0200

    Remove unecessary use clause

--- src/ado-sessions-sources.adb
+++ src/ado-sessions-sources.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  ADO Databases -- Database Connections
---  Copyright (C) 2010, 2011, 2012, 2013, 2017 Stephane Carrez
+--  Copyright (C) 2010, 2011, 2012, 2013, 2017, 2018 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,9 +17,6 @@
 -----------------------------------------------------------------------
 
 package body ADO.Sessions.Sources is
-
-   use ADO.Drivers;
-   use type ADO.Drivers.Connections.Database_Connection_Access;
 
    --  ------------------------------
    --  Set the master data source
