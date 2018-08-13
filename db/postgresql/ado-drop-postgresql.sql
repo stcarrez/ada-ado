16d960a796d1e3522ce591b5f1a08bcd5956fb3b
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-08-13 21:24:29 +0200

    SQL files to create the ADO specific tables

---
+++ db/postgresql/ado-drop-postgresql.sql
@@ -0,0 +1,3 @@
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS "sequence";
+DROP TABLE IF EXISTS "entity_type";
---
+++ db/postgresql/ado-postgresql.sql
@@ -0,0 +1,25 @@
+/* File generated automatically by dynamo */
+/* Entity types */
+CREATE TABLE entity_type (
+  /* the entity type identifier */
+  "id" INTEGER  AUTO_INCREMENT,
+  /* the entity type name (table name) */
+  "name" VARCHAR(127) UNIQUE NOT NULL,
+  PRIMARY KEY ("id")
+);
+/* Sequence generator */
+CREATE TABLE sequence (
+  /* the sequence name */
+  "name" VARCHAR(127) NOT NULL,
+  /* the sequence record version */
+  "version" int ,
+  /* the sequence value */
+  "value" BIGINT ,
+  /* the sequence block size */
+  "block_size BIGINT ,
+  PRIMARY KEY ("name")
+);
+INSERT INTO entity_type (name) VALUES
+("entity_type")
+,("sequence")
+;
