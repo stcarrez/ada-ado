26480b31713b9b66637612147a6adc6eb5e3dc59
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-10-07 10:21:10 +0200

    Move the Configuration type in a separate package ADO.Configs so that it is easier to use

--- src/ado-drivers-connections.ads
+++ src/ado-drivers-connections.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  ADO Drivers -- Database Drivers
---  Copyright (C) 2010, 2011, 2012, 2016, 2017 Stephane Carrez
+--  Copyright (C) 2010, 2011, 2012, 2016, 2017, 2018 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,8 +20,9 @@ with Ada.Finalization;
 
 with ADO.Statements;
 with ADO.Schemas;
-with Util.Properties;
+with ADO.Configs;
 with Util.Strings;
+with Util.Strings.Vectors;
 with Util.Refs;
 
 --  The <b>ADO.Drivers</b> package represents the database driver that will create
@@ -81,6 +82,12 @@ package ADO.Drivers.Connections is
    procedure Load_Schema (Database : in Database_Connection;
                           Schema   : out ADO.Schemas.Schema_Definition) is abstract;
 
+   --  Create the database and initialize it with the schema SQL file.
+   procedure Create_Database (Database    : in Database_Connection;
+                              Config      : in Configs.Configuration'Class;
+                              Schema_Path : in String;
+                              Messages    : out Util.Strings.Vectors.Vector) is abstract;
+
    --  Get the database driver which manages this connection.
    function Get_Driver (Database : in Database_Connection)
                         return Driver_Access is abstract;
@@ -95,57 +102,10 @@ package ADO.Drivers.Connections is
    --  ------------------------------
    --  The database configuration properties
    --  ------------------------------
-   type Configuration is new Ada.Finalization.Controlled with private;
-   type Configuration_Access is access all Configuration'Class;
+   type Configuration is new ADO.Configs.Configuration with null record;
 
-   --  Set the connection URL to connect to the database.
-   --  The driver connection is a string of the form:
-   --
-   --   driver://[host][:port]/[database][?property1][=value1]...
-   --
-   --  If the string is invalid or if the driver cannot be found,
-   --  the Connection_Error exception is raised.
-   procedure Set_Connection (Controller : in out Configuration;
-                             URI        : in String);
-
-   --  Set a property on the datasource for the driver.
-   --  The driver can retrieve the property to configure and open
-   --  the database connection.
-   procedure Set_Property (Controller : in out Configuration;
-                           Name       : in String;
-                           Value      : in String);
-
-   --  Get a property from the data source configuration.
-   --  If the property does not exist, an empty string is returned.
-   function Get_Property (Controller : in Configuration;
-                          Name       : in String) return String;
-
-   --  Set the server hostname.
-   procedure Set_Server (Controller : in out Configuration;
-                         Server     : in String);
-
-   --  Get the server hostname.
-   function Get_Server (Controller : in Configuration) return String;
-
-   --  Set the server port.
-   procedure Set_Port (Controller : in out Configuration;
-                       Port       : in Natural);
-
-   --  Get the server port.
-   function Get_Port (Controller : in Configuration) return Natural;
-
-   --  Set the database name.
-   procedure Set_Database (Controller : in out Configuration;
-                           Database   : in String);
-
-   --  Get the database name.
-   function Get_Database (Controller : in Configuration) return String;
-
-   --  Get the database driver name.
-   function Get_Driver (Controller : in Configuration) return String;
-
-   --  Get the database driver index.
-   function Get_Driver (Controller : in Configuration) return Driver_Index;
+   --  Get the driver index that corresponds to the driver for this database connection string.
+   function Get_Driver (Config : in Configuration) return Driver_Index;
 
    --  Create a new connection using the configuration parameters.
    procedure Create_Connection (Config : in Configuration'Class;
@@ -177,16 +137,6 @@ private
    type Driver is abstract new Ada.Finalization.Limited_Controlled with record
       Name  : Util.Strings.Name_Access;
       Index : Driver_Index;
-   end record;
-
-   type Configuration is new Ada.Finalization.Controlled with record
-      URI        : Unbounded_String;
-      Log_URI    : Unbounded_String;
-      Server     : Unbounded_String;
-      Port       : Natural := 0;
-      Database   : Unbounded_String;
-      Properties : Util.Properties.Manager;
-      Driver     : Driver_Access;
    end record;
 
 end ADO.Drivers.Connections;
