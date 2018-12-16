794c696c277f8df91ceec5a77bc0a373af2224ea
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-12-16 18:15:00 +0100

    Declare the Set_Audit_Manager procedure

--- regtests/regtests.ads
+++ regtests/regtests.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
---  ADO Tests -- Database sequence generator
---  Copyright (C) 2009, 2010, 2017 Stephane Carrez
+--  regtests -- Support for unit tests
+--  Copyright (C) 2009, 2010, 2017, 2018 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,28 +16,24 @@
 --  limitations under the License.
 -----------------------------------------------------------------------
 
+with ADO.Audits;
 with ADO.Sessions;
 with ADO.Sessions.Sources;
 package Regtests is
 
-   --  ------------------------------
    --  Get the database manager to be used for the unit tests
-   --  ------------------------------
    function Get_Controller return ADO.Sessions.Sources.Data_Source'Class;
 
-   --  ------------------------------
    --  Get the readonly connection database to be used for the unit tests
-   --  ------------------------------
    function Get_Database return ADO.Sessions.Session;
 
-   --  ------------------------------
    --  Get the writeable connection database to be used for the unit tests
-   --  ------------------------------
    function Get_Master_Database return ADO.Sessions.Master_Session;
 
-   -- ------------------------------
+   --  Set the audit manager on the factory.
+   procedure Set_Audit_Manager (Manager : in ADO.Audits.Audit_Manager_Access);
+
    --  Initialize the test database
-   -- ------------------------------
    procedure Initialize (Name : in String);
 
 end Regtests;
