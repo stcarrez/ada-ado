9bcdf10f0529190b5c6b8071827361042feb72cf
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-12-16 10:02:15 +0100

    Database audit support: new package for table auditing

---
+++ src/ado-audits.ads
@@ -0,0 +1,88 @@
+-----------------------------------------------------------------------
+--  ado-audits -- Auditing support
+--  Copyright (C) 2018 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Util.Beans.Objects;
+
+with ADO.Configs;
+with ADO.Objects;
+with ADO.Schemas;
+with ADO.Sessions;
+package ADO.Audits is
+
+   subtype Object_Key_Type is ADO.Objects.Object_Key_Type;
+   subtype Auditable_Class_Mapping is ADO.Schemas.Auditable_Class_Mapping;
+   subtype Auditable_Class_Mapping_Access is ADO.Schemas.Auditable_Class_Mapping_Access;
+   subtype Class_Mapping_Access is ADO.Schemas.Class_Mapping_Access;
+
+   package UBO renames Util.Beans.Objects;
+
+   --  The `Audit_Info` describes a column field that is modified.
+   type Audit_Info is limited record
+      Field     : ADO.Schemas.Column_Index := 0;
+      Old_Value : UBO.Object;
+      New_Value : UBO.Object;
+   end record;
+
+   type Audit_Index is new Positive range 1 .. ADO.Configs.MAX_COLUMNS;
+
+   type Audit_Array is array (Audit_Index range <>) of Audit_Info;
+
+   --  The `Auditable_Object_Record` is the root type of any auditable database record.
+   --  It inherit from the `Object_Record` and adds auditing support by defining the
+   --  database column fields which can be audited.  When a field is modified, it holds
+   --  the audit information that tracks the old and new values.
+   type Auditable_Object_Record (Key_Type   : Object_Key_Type;
+                                 Of_Class   : Class_Mapping_Access;
+                                 With_Audit : Auditable_Class_Mapping_Access) is abstract
+       new ADO.Objects.Object_Record with private;
+
+   --  Release the object.
+   overriding
+   procedure Finalize (Object : in out Auditable_Object_Record);
+
+   --  Record an audit information for a field.
+   procedure Audit_Field (Object    : in out Auditable_Object_Record;
+                          Field     : in ADO.Schemas.Column_Index;
+                          Old_Value : in UBO.Object;
+                          New_Value : in UBO.Object);
+
+   --  The `Audit_Manager` is the interface of the audit manager component that is responsible
+   --  for saving the audit information in the database.
+   type Audit_Manager is limited interface;
+
+   --  Save the audit changes in the database.
+   procedure Save (Manager : in out Audit_Manager;
+                   Session : in out ADO.Sessions.Master_Session'Class;
+                   Object  : in Auditable_Object_Record'Class;
+                   Changes : in Audit_Array) is abstract;
+
+   procedure Save (Session : in out ADO.Sessions.Master_Session'Class;
+                   Object  : in out Auditable_Object_Record'Class);
+
+private
+
+   type Audit_Array_Access is access all Audit_Array;
+
+   type Auditable_Object_Record (Key_Type   : Object_Key_Type;
+                                 Of_Class   : Class_Mapping_Access;
+                                 With_Audit : Auditable_Class_Mapping_Access) is abstract
+   new ADO.Objects.Object_Record (Key_Type => Key_Type, Of_Class => Of_Class) with record
+      Audits : Audit_Array_Access;
+   end record;
+
+end ADO.Audits;
