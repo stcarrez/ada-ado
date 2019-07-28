ab6b151a1d44cc2fdc01e7aa7a1a7e8ee7d35298
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-07-28 23:08:15 +0200

    Declare Write_Query procedure to serialize the result of a Query_Statement

--- src/ado-utils-serialize.ads
+++ src/ado-utils-serialize.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  ado-utils-serialize -- Utility operations for JSON/XML serialization
---  Copyright (C) 2016 Stephane Carrez
+--  Copyright (C) 2016, 2019 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,6 +18,7 @@
 
 with Util.Serialize.IO;
 with ADO.Objects;
+with ADO.Statements;
 
 package ADO.Utils.Serialize is
 
@@ -30,5 +31,10 @@ package ADO.Utils.Serialize is
    procedure Write_Entity (Stream : in out Util.Serialize.IO.Output_Stream'Class;
                            Name   : in String;
                            Value  : in ADO.Objects.Object_Key);
+
+   --  Write the SQL query results to the serialization stream (JSON/XML).
+   procedure Write_Query (Stream : in out Util.Serialize.IO.Output_Stream'Class;
+                          Name   : in String;
+                          Query  : in out ADO.Statements.Query_Statement);
 
 end ADO.Utils.Serialize;
