c2c237cd3b4e1c0987bee9aa719898e4033e86b7
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-14 23:40:09 +0100

    Add --quiet for coverage

--- coverage.sh
+++ coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 NAME=ado.cov
-lcov --base-directory . --directory . -c --include "*/ada-ado/src/*" -o $NAME
+lcov --quiet --base-directory . --directory . -c --include "*/ada-ado/src/*" -o $NAME
 rm -rf cover
-genhtml --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
+genhtml --quiet --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
  
