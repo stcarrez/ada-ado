69d9cb92f1dd7c157a1c7a7d8482f74b2d2d5aed
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-15 14:28:13 +0100

    Revert to avoid using --include and --exclude option since old version of lcov do not support these options

--- coverage.sh
+++ coverage.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 NAME=ado.cov
-lcov --quiet --base-directory . --directory . -c --include "*/ada-ado/src/*" -o $NAME
+lcov --quiet --base-directory . --directory . -c -o $NAME
+lcov --quiet --remove $NAME "/usr*" -o $NAME
+lcov --quiet --remove $NAME "/build*" -o $NAME
+lcov --quiet --remove $NAME "/opt*" -o $NAME
+lcov --quiet --remove $NAME "*/adainclude*" -o $NAME
+lcov --quiet --remove $NAME "*/regtests*" -o $NAME
+lcov --quiet --remove $NAME "*/b__*" -o $NAME
 rm -rf cover
 genhtml --quiet --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
  
