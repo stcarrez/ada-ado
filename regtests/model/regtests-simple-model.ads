6a4dcce65d5bf4d9bb969b0293f3b9bab448e615
Stephane Carrez <Stephane.Carrez@gmail.com>
2021-06-26 09:55:32 +0200

    Rebuild the Ada files

--- regtests/model/regtests-simple-model.ads
+++ regtests/model/regtests-simple-model.ads
@@ -37,6 +37,8 @@ package Regtests.Simple.Model is
 
    type Allocate_Ref is new ADO.Objects.Object_Ref with null record;
 
+   type Keys_Ref is new ADO.Objects.Object_Ref with null record;
+
    type User_Ref is new ADO.Objects.Object_Ref with null record;
 
    --  --------------------
@@ -128,6 +130,137 @@ package Regtests.Simple.Model is
    subtype Allocate_Vector is Allocate_Vectors.Vector;
 
    procedure List (Object  : in out Allocate_Vector;
+                   Session : in out ADO.Sessions.Session'Class;
+                   Query   : in ADO.SQL.Query'Class);
+   --  --------------------
+   --  Record representing a user
+   --  --------------------
+   --  Create an object key for Keys.
+   function Keys_Key (Id : in ADO.Identifier) return ADO.Objects.Object_Key;
+   --  Create an object key for Keys from a string.
+   --  Raises Constraint_Error if the string cannot be converted into the object key.
+   function Keys_Key (Id : in String) return ADO.Objects.Object_Key;
+
+   Null_Keys : constant Keys_Ref;
+   function "=" (Left, Right : Keys_Ref'Class) return Boolean;
+
+   --  Set the user id
+   procedure Set_Id (Object : in out Keys_Ref;
+                     Value  : in Ada.Strings.Unbounded.Unbounded_String);
+   procedure Set_Id (Object : in out Keys_Ref;
+                     Value : in String);
+
+   --  Get the user id
+   function Get_Id (Object : in Keys_Ref)
+                 return Ada.Strings.Unbounded.Unbounded_String;
+   function Get_Id (Object : in Keys_Ref)
+                 return String;
+   --  Get the allocate version.
+   function Get_Version (Object : in Keys_Ref)
+                 return Integer;
+
+   --  Set the name
+   procedure Set_Name (Object : in out Keys_Ref;
+                       Value  : in ADO.Nullable_String);
+   procedure Set_Name (Object : in out Keys_Ref;
+                       Value : in String);
+
+   --  Get the name
+   function Get_Name (Object : in Keys_Ref)
+                 return ADO.Nullable_String;
+   function Get_Name (Object : in Keys_Ref)
+                 return String;
+
+   --  Set the cost
+   procedure Set_Cost (Object : in out Keys_Ref;
+                       Value  : in Float);
+
+   --  Get the cost
+   function Get_Cost (Object : in Keys_Ref)
+                 return Float;
+
+   --  Set the total
+   procedure Set_Total (Object : in out Keys_Ref;
+                        Value  : in Long_Float);
+
+   --  Get the total
+   function Get_Total (Object : in Keys_Ref)
+                 return Long_Float;
+
+   --  Set the user
+   procedure Set_User (Object : in out Keys_Ref;
+                       Value  : in ADO.Identifier);
+
+   --  Get the user
+   function Get_User (Object : in Keys_Ref)
+                 return ADO.Identifier;
+
+   --  Set the second key
+   procedure Set_Second_Key (Object : in out Keys_Ref;
+                             Value  : in Ada.Strings.Unbounded.Unbounded_String);
+   procedure Set_Second_Key (Object : in out Keys_Ref;
+                             Value : in String);
+
+   --  Get the second key
+   function Get_Second_Key (Object : in Keys_Ref)
+                 return Ada.Strings.Unbounded.Unbounded_String;
+   function Get_Second_Key (Object : in Keys_Ref)
+                 return String;
+
+   --  Load the entity identified by 'Id'.
+   --  Raises the NOT_FOUND exception if it does not exist.
+   procedure Load (Object  : in out Keys_Ref;
+                   Session : in out ADO.Sessions.Session'Class;
+                   Id      : in Ada.Strings.Unbounded.Unbounded_String);
+
+   --  Load the entity identified by 'Id'.
+   --  Returns True in <b>Found</b> if the object was found and False if it does not exist.
+   procedure Load (Object  : in out Keys_Ref;
+                   Session : in out ADO.Sessions.Session'Class;
+                   Id      : in Ada.Strings.Unbounded.Unbounded_String;
+                   Found   : out Boolean);
+
+   --  Find and load the entity.
+   overriding
+   procedure Find (Object  : in out Keys_Ref;
+                   Session : in out ADO.Sessions.Session'Class;
+                   Query   : in ADO.SQL.Query'Class;
+                   Found   : out Boolean);
+
+   --  Save the entity.  If the entity does not have an identifier, an identifier is allocated
+   --  and it is inserted in the table.  Otherwise, only data fields which have been changed
+   --  are updated.
+   overriding
+   procedure Save (Object  : in out Keys_Ref;
+                   Session : in out ADO.Sessions.Master_Session'Class);
+
+   --  Delete the entity.
+   overriding
+   procedure Delete (Object  : in out Keys_Ref;
+                     Session : in out ADO.Sessions.Master_Session'Class);
+
+   overriding
+   function Get_Value (From : in Keys_Ref;
+                       Name : in String) return Util.Beans.Objects.Object;
+
+   --  Table definition
+   KEYS_TABLE : constant ADO.Schemas.Class_Mapping_Access;
+
+   --  Internal method to allocate the Object_Record instance
+   overriding
+   procedure Allocate (Object : in out Keys_Ref);
+
+   --  Copy of the object.
+   procedure Copy (Object : in Keys_Ref;
+                   Into   : in out Keys_Ref);
+
+   package Keys_Vectors is
+      new Ada.Containers.Vectors (Index_Type   => Positive,
+                                  Element_Type => Keys_Ref,
+                                  "="          => "=");
+   subtype Keys_Vector is Keys_Vectors.Vector;
+
+   procedure List (Object  : in out Keys_Vector;
                    Session : in out ADO.Sessions.Session'Class;
                    Query   : in ADO.SQL.Query'Class);
    --  --------------------
@@ -304,22 +437,93 @@ private
 
    procedure Set_Field (Object : in out Allocate_Ref'Class;
                         Impl   : out Allocate_Access);
-   USER_NAME : aliased constant String := "test_user";
+   KEYS_NAME : aliased constant String := "test_keys";
    COL_0_2_NAME : aliased constant String := "id";
    COL_1_2_NAME : aliased constant String := "version";
-   COL_2_2_NAME : aliased constant String := "value";
-   COL_3_2_NAME : aliased constant String := "name";
-   COL_4_2_NAME : aliased constant String := "select";
+   COL_2_2_NAME : aliased constant String := "name";
+   COL_3_2_NAME : aliased constant String := "cost";
+   COL_4_2_NAME : aliased constant String := "total";
+   COL_5_2_NAME : aliased constant String := "user_id";
+   COL_6_2_NAME : aliased constant String := "key_id";
 
-   USER_DEF : aliased constant ADO.Schemas.Class_Mapping :=
-     (Count   => 5,
-      Table   => USER_NAME'Access,
+   KEYS_DEF : aliased constant ADO.Schemas.Class_Mapping :=
+     (Count   => 7,
+      Table   => KEYS_NAME'Access,
       Members => (
          1 => COL_0_2_NAME'Access,
          2 => COL_1_2_NAME'Access,
          3 => COL_2_2_NAME'Access,
          4 => COL_3_2_NAME'Access,
-         5 => COL_4_2_NAME'Access)
+         5 => COL_4_2_NAME'Access,
+         6 => COL_5_2_NAME'Access,
+         7 => COL_6_2_NAME'Access)
+     );
+   KEYS_TABLE : constant ADO.Schemas.Class_Mapping_Access
+      := KEYS_DEF'Access;
+
+
+   Null_Keys : constant Keys_Ref
+      := Keys_Ref'(ADO.Objects.Object_Ref with null record);
+
+   type Keys_Impl is
+      new ADO.Objects.Object_Record (Key_Type => ADO.Objects.KEY_STRING,
+                                     Of_Class => KEYS_DEF'Access)
+   with record
+       Version : Integer;
+       Name : ADO.Nullable_String;
+       Cost : Float;
+       Total : Long_Float;
+       User : ADO.Identifier;
+       Second_Key : Ada.Strings.Unbounded.Unbounded_String;
+   end record;
+
+   type Keys_Access is access all Keys_Impl;
+
+   overriding
+   procedure Destroy (Object : access Keys_Impl);
+
+   overriding
+   procedure Find (Object  : in out Keys_Impl;
+                   Session : in out ADO.Sessions.Session'Class;
+                   Query   : in ADO.SQL.Query'Class;
+                   Found   : out Boolean);
+
+   overriding
+   procedure Load (Object  : in out Keys_Impl;
+                   Session : in out ADO.Sessions.Session'Class);
+   procedure Load (Object  : in out Keys_Impl;
+                   Stmt    : in out ADO.Statements.Query_Statement'Class;
+                   Session : in out ADO.Sessions.Session'Class);
+
+   overriding
+   procedure Save (Object  : in out Keys_Impl;
+                   Session : in out ADO.Sessions.Master_Session'Class);
+
+   procedure Create (Object  : in out Keys_Impl;
+                     Session : in out ADO.Sessions.Master_Session'Class);
+
+   overriding
+   procedure Delete (Object  : in out Keys_Impl;
+                     Session : in out ADO.Sessions.Master_Session'Class);
+
+   procedure Set_Field (Object : in out Keys_Ref'Class;
+                        Impl   : out Keys_Access);
+   USER_NAME : aliased constant String := "test_user";
+   COL_0_3_NAME : aliased constant String := "id";
+   COL_1_3_NAME : aliased constant String := "version";
+   COL_2_3_NAME : aliased constant String := "value";
+   COL_3_3_NAME : aliased constant String := "name";
+   COL_4_3_NAME : aliased constant String := "select";
+
+   USER_DEF : aliased constant ADO.Schemas.Class_Mapping :=
+     (Count   => 5,
+      Table   => USER_NAME'Access,
+      Members => (
+         1 => COL_0_3_NAME'Access,
+         2 => COL_1_3_NAME'Access,
+         3 => COL_2_3_NAME'Access,
+         4 => COL_3_3_NAME'Access,
+         5 => COL_4_3_NAME'Access)
      );
    USER_TABLE : constant ADO.Schemas.Class_Mapping_Access
       := USER_DEF'Access;
