1393cba037165e223560d9c63d6bd6fd3222a593
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-06-11 13:45:25 +0200

    Fix compilation warning with GCC 12: remove unused with clause for an ancestor

--- src/ado-objects-cache.ads
+++ src/ado-objects-cache.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  objects.cache -- Object cache
---  Copyright (C) 2009, 2010 Stephane Carrez
+--  Copyright (C) 2009, 2010, 2022 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,7 +18,6 @@
 
 with Ada.Finalization;
 
-with ADO.Objects;
 with Ada.Containers.Indefinite_Hashed_Sets;
 
 --  The <b>ADO.Objects.Cache</b> holds a cache of object records.
