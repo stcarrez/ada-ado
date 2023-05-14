cbd264d0ddc3361dacb74db1762225c8d708b5f4
Stephane Carrez <Stephane.Carrez@gmail.com>
2023-05-14 10:14:50 +0200

    Cleanup the coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 NAME=ado.cov
-lcov --quiet --base-directory . --directory . -c -o $NAME
-lcov --quiet --remove $NAME "/usr*" -o $NAME
-lcov --quiet --remove $NAME "/build*" -o $NAME
-lcov --quiet --remove $NAME "/opt*" -o $NAME
-lcov --quiet --remove $NAME "*/adainclude*" -o $NAME
-lcov --quiet --remove $NAME "*/regtests*" -o $NAME
-lcov --quiet --remove $NAME "*/b__*" -o $NAME
+lcov --quiet --base-directory . --directory . \
+   --no-external \
+   --exclude '*/b__*.adb' \
+   --exclude '*/samples/*' \
+   --exclude '*/regtests*' -c -o $NAME
 rm -rf cover
 genhtml --quiet --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
  
