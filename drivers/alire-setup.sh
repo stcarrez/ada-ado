3d71e4dbddeb41be84156b56708dd93a16df2153
Stephane Carrez <Stephane.Carrez@gmail.com>
2024-09-22 15:30:19 +0200

    Fix alire-setup.sh to re-build the gnatprep phase if the target does not exist (useful after running make clean to build again)

--- drivers/alire-setup.sh
+++ drivers/alire-setup.sh
@@ -8,9 +8,12 @@
 ) > ado_all-new.def
 if test -f ado_all.def && cmp ado_all-new.def ado_all.def; then
   rm -f ado_all-new.def
-  exit 0
+  if test -f src/ado-drivers-initialize.adb && test -f ado_all.gpr; then
+    exit 0
+  fi
+else
+  mv ado_all-new.def ado_all.def
 fi
-mv ado_all-new.def ado_all.def
 gnatprep src/ado-drivers-initialize.gpb src/ado-drivers-initialize.adb ado_all.def
 gnatprep ado_all.gpg ado_all.gpr ado_all.def
 
