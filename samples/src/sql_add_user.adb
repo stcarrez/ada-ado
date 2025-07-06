1edb1994b19d1ebc4b520b5226e1026fb74371c3
Stephane Carrez <Stephane.Carrez@gmail.com>
2025-07-06 19:54:33 +0200

    Feature #29: add examples with direct SQL queries
    - add sql_list_user.adb which is similar to list_user.adb
      but uses direct SQL queries
    - add sql_add_user.adb to insert users manually with
      direct SQL statement (and not with the ORM)

---
+++ samples/src/sql_add_user.adb
@@ -0,0 +1,92 @@
+-----------------------------------------------------------------------
+--  Add_User -- Example to add an object in the database
+--  Copyright (C) 2010 - 2025 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--  SPDX-License-Identifier: Apache-2.0
+-----------------------------------------------------------------------
+
+with ADO;
+with ADO.Drivers;
+with ADO.Configs;
+with ADO.Sessions;
+with ADO.Connections;
+with ADO.Statements;
+with ADO.Sessions.Factory;
+
+with Util.Strings;
+with Util.Log.Loggers;
+
+with Ada.Text_IO;
+with Ada.Exceptions;
+with Ada.Command_Line;
+with Ada.Calendar;
+procedure Sql_Add_User is
+
+   function Get_Name (Email : in String) return String;
+
+   Factory    : ADO.Sessions.Factory.Session_Factory;
+
+   function Get_Name (Email : in String) return String is
+      Pos : constant Natural := Util.Strings.Index (Email, '@');
+   begin
+      if Pos > 0 then
+         return Email (Email'First .. Pos - 1);
+      else
+         return Email;
+      end if;
+   end Get_Name;
+
+begin
+   Util.Log.Loggers.Initialize ("samples.properties");
+
+   --  Initialize the database drivers.
+   ADO.Drivers.Initialize ("samples.properties");
+
+   if Ada.Command_Line.Argument_Count < 1 then
+      Ada.Text_IO.Put_Line ("Usage: add_user user-email ...");
+      Ada.Text_IO.Put_Line ("Example: add_user joe.potter@gmail.com");
+      Ada.Command_Line.Set_Exit_Status (2);
+      return;
+   end if;
+
+   --  Initialize the session factory to connect to the
+   --  database defined by 'ado.database' property.
+   Factory.Create (ADO.Configs.Get_Config ("ado.database"));
+
+   declare
+      DB    : ADO.Sessions.Master_Session := Factory.Get_Master_Session;
+      Stmt  : ADO.Statements.Insert_Statement;
+      Query : constant String := "INSERT INTO user (`id`, `object_version`,"
+        & " `name`, `email`, `date`, `description`, `status`) VALUES(?, ?, "
+        & "?, ?, ?, ?, ?);";
+   begin
+      DB.Begin_Transaction;
+      Stmt := DB.Create_Statement (Query);
+      for I in 1 .. Ada.Command_Line.Argument_Count loop
+         declare
+            Email : constant String := Ada.Command_Line.Argument (I);
+            Name  : constant String := Get_Name (Email);
+            Id    : ADO.Identifier := 1;
+            Desc  : constant String := "";
+            Status : Natural := 0;
+            Result : Integer;
+         begin
+            DB.Allocate ("user", Id);
+            Stmt.Bind_Param ("id", Id);
+            Stmt.Bind_Param ("version", Natural (1));
+            Stmt.Bind_Param ("name", Name);
+            Stmt.Bind_Param ("email", Email);
+            Stmt.Bind_Param ("date", Ada.Calendar.Clock);
+            Stmt.Bind_Param ("description", Desc);
+            Stmt.Bind_Param ("status", Status);
+            Stmt.Execute (Result);
+         end;
+      end loop;
+      DB.Commit;
+   end;
+
+exception
+   when E : ADO.Connections.Database_Error | ADO.Sessions.Connection_Error =>
+      Ada.Text_IO.Put_Line ("Cannot connect to database: "
+                              & Ada.Exceptions.Exception_Message (E));
+end Sql_Add_User;
---
+++ samples/src/sql_list_user.adb
@@ -0,0 +1,62 @@
+-----------------------------------------------------------------------
+--  List_User -- Example of SQL queries
+--  Copyright (C) 2010 - 2025 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--  SPDX-License-Identifier: Apache-2.0
+-----------------------------------------------------------------------
+
+with ADO;
+with ADO.Drivers;
+with ADO.Configs;
+with ADO.Sessions;
+with ADO.Connections;
+with ADO.Statements;
+with ADO.Sessions.Factory;
+
+with Util.Log.Loggers;
+
+with Ada.Text_IO;
+with Ada.Exceptions;
+procedure Sql_List_User is
+   Factory    : ADO.Sessions.Factory.Session_Factory;
+begin
+   Util.Log.Loggers.Initialize ("samples.properties");
+
+   --  Initialize the database drivers.
+   ADO.Drivers.Initialize ("samples.properties");
+
+   --  Initialize the session factory to connect to the
+   --  database defined by 'ado.database' property.
+   Factory.Create (ADO.Configs.Get_Config ("ado.database"));
+
+   declare
+      DB      : constant ADO.Sessions.Session := Factory.Get_Session;
+      Stmt    : ADO.Statements.Query_Statement;
+      Count   : Natural := 0;
+      Query   : constant String := "SELECT u.id, u.name, u.email FROM user AS u"
+        & " ORDER BY u.name ASC";
+   begin
+      Stmt := DB.Create_Statement (Query);
+      Stmt.Execute;
+      while Stmt.Has_Elements loop
+         Ada.Text_IO.Put (ADO.Identifier'Image (Stmt.Get_Identifier (0)));
+         Ada.Text_IO.Set_Col (10);
+         Ada.Text_IO.Put (Stmt.Get_String (1));
+         Ada.Text_IO.Set_Col (60);
+         Ada.Text_IO.Put_Line (Stmt.Get_String (2));
+         Stmt.Next;
+         Count := Count + 1;
+      end loop;
+
+      --  Print the list
+      if Count = 0 then
+         Ada.Text_IO.Put_Line ("User info list is empty.");
+         Ada.Text_IO.Put_Line ("Use the 'add_user' to add users in the table.");
+      end if;
+   end;
+
+exception
+   when E : ADO.Connections.Database_Error | ADO.Sessions.Connection_Error =>
+      Ada.Text_IO.Put_Line ("Cannot connect to database: "
+                              & Ada.Exceptions.Exception_Message (E));
+end Sql_List_User;
